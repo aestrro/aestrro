e87f1948b7596805ca19898da2dacb11affe3540
Kevin Carr <aestrro@gmail.com>
2022-01-29 14:27:49 -0800

    Remove for Public Profile

--- gatsby-browser.js
+++
@@ -1,8 +0,0 @@
-import('./src/assets/styles/index.scss')
-import PropTypes from 'prop-types'
-
-export const wrapRootElement = ({ element }) => element
-
-wrapRootElement.propTypes = {
-    element: PropTypes.object,
-}
